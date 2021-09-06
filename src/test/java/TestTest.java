3a77a94ed789c5d35f83f4f8280d3cef3fcac9d7
zotyo911 <sooszoltan79@gmail.com>
2021-09-06 07:31:53 +0200

    probe

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestTest {
+
+    WebDriver webdriver;
+
+    @BeforeEach
+    public void Setup()
+    {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        webdriver = new ChromeDriver(options);
+
+        webdriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        webdriver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void Close()
+    {
+        //Driver dispose
+        webdriver.quit();
+    }
+
+    @Test
+    public void firstTest(){
+        webdriver.get("https://www.seleniumeasy.com/");
+
+        Assertions.assertEquals("https://www.seleniumeasy.com/", webdriver.getCurrentUrl());
+    }
+}
